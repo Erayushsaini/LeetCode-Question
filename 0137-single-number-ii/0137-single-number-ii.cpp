5fbd7e0d3f6e283f66bfec3e2146a5eb620f2f3c
Erayushsaini <erayushsaini04@gmail.com>
2026-03-25 16:30:11 +0530

    Time: 0 ms (100%), Space: 13.3 MB (85.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> map;
-
-        for(int val:nums) {
-            map[val]++;
-        }
-        for(auto p:map) {
-            if(p.second==1) {
-                return p.first;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();){
+            if(i+1<nums.size() && i+2<nums.size() && nums[i]==nums[i+1] && nums[i]==nums[i+1]) {
+                i=i+3;
+            }
+            else{
+                return nums[i];
             }
         }
-        return -1;
+        return nums[nums.size()-1];
     }
 };
