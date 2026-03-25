0b08badcf5918ccb061352a8700be49752f87f6e
Erayushsaini <erayushsaini04@gmail.com>
2026-03-25 17:01:12 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.04%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
+        //time complexity is o(n) and s.c is o(1)
         int ans=0;
         for(int val:nums){
             ans^=val;
         }
         return ans;
+
+        //other method is using hash map  in this o(n)time complexity and s.c is o(n)and sorting t.c(o(nlogn)) and space complexity is o(1)
+        // unordered_map<int,int> map;
+
+        // for(int i=0;i<nums.size();i++) {
+        //     map[nums[i]]++;
+        // }
+        // for(auto p:map) {
+        //     if(p.second==1) {
+        //         return p.first;
+        //     }
+        // }
+        // return -1;
     }        
 };
