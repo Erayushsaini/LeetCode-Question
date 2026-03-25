840e95c077e7a70bb32e045844cbd0d33f4b6358
Erayushsaini <erayushsaini04@gmail.com>
2026-03-25 20:53:18 +0530

    Time: 3 ms (32.9%), Space: 13.3 MB (85.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;){  //or can do nums.size() so in the if condition change is 
-            if(nums[i]==nums[i+1] && nums[i]==nums[i+1]) { //i+1<nums.size() && i+2<nums.size() && then other written line 
-                i=i+3;
+        //solution using the bit manupulation 
+        int ans=0;
+        for(int i=0;i<32;i++) {
+            int count=0;
+            for(int j=0;j<nums.size();j++) {
+                if(((nums[j]>>i)&1)==1) {
+                    count++;
+                }
             }
-            else{
-                return nums[i];
+            if(count%3!=0) {
+                ans=ans|(1<<i);
             }
         }
-        return nums[nums.size()-1]; //this is the edge case 
+        return ans;
+
+
+
+
+
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;){  //or can do nums.size() so in the if condition change is 
+        //     if(nums[i]==nums[i+1] && nums[i]==nums[i+1]) { //i+1<nums.size() && i+2<nums.size() && then other written line 
+        //         i=i+3;
+        //     }
+        //     else{
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[nums.size()-1]; //this is the edge case 
 
 
         //other solution by  using map but t.c is o(n) and space complexity is o(n)
