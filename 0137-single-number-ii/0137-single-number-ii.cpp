0a428b4399b7570a3fd08185b5b22850c027b59b
Erayushsaini <erayushsaini04@gmail.com>
2026-03-25 16:37:54 +0530

    Time: 0 ms (100%), Space: 13.2 MB (85.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;){
-            if(nums[i]==nums[i+1] && nums[i]==nums[i+1]) {
+        for(int i=0;i<nums.size()-1;){  //or can do nums.size() so in the if condition change is 
+            if(nums[i]==nums[i+1] && nums[i]==nums[i+1]) { //i+1<nums.size() && i+2<nums.size() && then other written line 
                 i=i+3;
             }
             else{
